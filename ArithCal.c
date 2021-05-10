b431f538c0ea92c95fe1c2c8a23f454555a8c507
Krushna Namdev Bhalke <49978990+knbhlke999@users.noreply.github.com>
2021-05-10 14:02:50 +0530

    Update ArithCal.c

--- ArithCal.c
+++ ArithCal.c
@@ -1,56 +1,12 @@
 //Study of "if"-"else if"-"else"
 //Basic Arithmetic Calculator
-//see : else if.png
-//refer : conditional branching.txt
+
 
 #include <stdio.h>
 
 int main()
 {
-/*float n1, n2;
-    float ans;
-    int choice;
 
-    printf("Enter 2 numbers ");
-    scanf("%f%f", &n1, &n2);
-
-    //menu
-    printf("\n 1. Addition");
-    printf("\n 2. Subtraction");
-    printf("\n 3. Multiplication");
-    printf("\n 4. Division");
-    printf("\n Enter Choice ");
-
-    scanf("%d", &choice);
-    //now choice would be : 1, 2, 3, 4 or anything else
-    //compare for equality
-    //and operate accordingly
-
-    if(choice == 1)
-    {//addition
-        ans = n1 + n2;
-        printf("\n %f + %f = %f ", n1, n2, ans);
-    }
-    else if(choice == 2)
-    {//subtraction
-        ans = n1- n2;
-        printf("\n %.2f - %.2f = %.2f ", n1, n2, ans);
-    }
-    else if(choice == 3)
-    {//multiplication
-        ans = n1 * n2;
-        printf("\n %.2f * %.2f = %.2f ", n1, n2, ans);
-    }
-    else if(choice == 4)
-    {//Division
-        ans = n1 / n2;
-        printf("\n %.2f / %.2f = %.2f ", n1, n2, ans);
-    }
-    else
-    {
-        printf("\n Wrong Choice");
-    }
-    return 0; */
 
     double num1;
     double num2;
@@ -91,5 +47,7 @@ int main()
     {
         printf("Invalid Operator");
     }
+    
+    retunr 0;
 
 }
